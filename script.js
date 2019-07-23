891c395619cad041b065ba7e9c6c8eb46bf9d769
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-23 11:31:48 +0200

    Milestone 1

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
 $( document ).ready(function() {
-  
+  $("#sendmsg").click(
+    function () {
+      // Salvo msg inserito dall'utente
+      var msg = $("#msginput").val();
+      // Inseriamo in pagina il messaggio
+      $(".chat").append(
+        "<div class='msguser'>" + msg + "</div>"
+      );
+      // Pulisco l'input alla fine
+      $("#msginput").val("");
+    }
+  );
 
 });
