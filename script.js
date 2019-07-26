aa069f7fa93a51f92c2a1d6d0924151199a40975
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-26 16:07:42 +0200

    1/2 Milestone 3

--- script.js
+++ script.js
@@ -1,49 +1,98 @@
 
 $( document ).ready(function() {
-  // BOTTA E RISPOSTA SULLA CHAT
+  // FUNZIONE BOTTA E RISPOSTA
+  function sendMessage() {
+    // Salvo msg inserito dall'utente
+    var msg = $("#msginput").val();
+    // Creo il clone del messaggio utente
+    var msgelement = $("#template .msguser").clone();
+    // Stampo l'input
+    msgelement.text(msg);
+    $(".boxchat.active").append(msgelement);
+    // Pulisco l'input alla fine
+    $("#msginput").val("");
+    // Creo il mio clone della risposta
+    var msgelementrisp = $("#template .msgrisp").clone();
+    // Creo risposta automatica in 1 secondo
+    setTimeout(function () {
+      var risp = msgelementrisp.text("Ok");
+      $(".boxchat.active").append(risp);
+    }, 1000);
+  }
+
+  // INVIO DEL MESSAGGIO TRAMITE IL CLICK
   $("#sendmsg").click(
     function () {
-      // Salvo msg inserito dall'utente
-      var msg = $("#msginput").val();
-      // Creo il clone del messaggio uetnete
-      var msgelement = $("#template .msguser").clone();
-      // Stampo l'input
-      msgelement.text(msg);
-      $(".chat").append(msgelement);
-      // Pulisco l'input alla fine
-      $("#msginput").val("");
-      // Creo il mio clone della risposta
-      var msgelementrisp = $("#template .msgrisp").clone();
-      // Creo risposta automatica in 1 secondo
-      setTimeout(function () {
-        var risp = msgelementrisp.text("Ok");
-        $(".chat").append(risp);
-      }, 1000);
+      sendMessage();
     }
   );
 
+  // INVIO DEL MESSAGGIO TRAMITE IL TASTO INVIO
+  $("#msginput").keypress(function() {
+      if (event.which == 13) {
+        sendMessage();
+      }
+    }
+  );
+
+  // FUNZIONE BOTTA E RISPOSTA
+  function sendMessage() {
+    // Salvo msg inserito dall'utente
+    var msg = $("#msginput").val();
+    // Creo il clone del messaggio utente
+    var msgelement = $("#template .msguser").clone();
+    // Stampo l'input
+    msgelement.text(msg);
+    $(".boxchat.active").append(msgelement);
+    // Pulisco l'input alla fine
+    $("#msginput").val("");
+    // Creo il mio clone della risposta
+    var msgelementrisp = $("#template .msgrisp").clone();
+    // Creo risposta automatica in 1 secondo
+    setTimeout(function () {
+      var risp = msgelementrisp.text("Ok");
+      $(".boxchat.active").append(risp);
+    }, 1000);
+  }
+
   // RICERCHIAMO I CONTATTI
   // La mia funzione si attiva premento un tasto sulla tastiera
-  $(".cerca").keyup(function () {
+  $(".cerca").keyup(
+    function () {
     // Creo una variabile e trasformo il valore in minuscolo
     var cercaNome = $(this).val().toLowerCase();
     // Nel momento in cui si digita i contatti spariscono
     $(".box-user > .user").hide();
-    // Se spazio > di 0 parte la funzione
-    if (cercaNome.length > 0) {
-      // Funzione che attraversa i mie contatti
-      $(".box-user > .user").each(function (){
-        // Creo una variabile che trasformo i nomi in minuscolo
-        var cercaCont = $(this).find(".name").text().toLowerCase();
-        // Verifico se inclusi
-        if (cercaCont.includes(cercaNome)) {
-          $(this).show();
-        }
-      });
-      // Se spazio = a 0 i contatti si vedono
-    } else if (cercaNome.length == 0) {
-      $(".box-user > .user").show();
+      // Se spazio > di 0 parte la funzione
+      if (cercaNome.length > 0) {
+        // Funzione che attraversa i mie contatti
+        $(".box-user > .user").each(function (){
+          // Creo una variabile che trasformo i nomi in minuscolo
+          var cercaCont = $(this).find(".name").text().toLowerCase();
+          // Verifico se inclusi
+          if (cercaCont.includes(cercaNome)) {
+            $(this).show();
+          }
+        });
+        // Se spazio = a 0 i contatti si vedono
+      } else if (cercaNome.length == 0) {
+        $(".box-user > .user").show();
+      }
     }
-  });
+  );
+
+  // CLICK SUL CONTATTO E RELATIVA CONVERSAZIONE
+  $(".user").click(
+    function () {
+      // Creo una variabile e assegno l'attributo su user
+      var refchat = $(this).attr("refchat");
+      // Rimuovo la classe active e attiva
+      $(".boxchat").removeClass("active");
+      $(".image").removeClass("attiva");
+      // Aggiungo la classe active e attiva dove c'è l'attributo
+      $('.boxchat[refchat="'+ refchat +'"]').addClass("active");
+      $('.image[refchat="'+ refchat +'"]').addClass("attiva");
+    }
+  );
 
 });
